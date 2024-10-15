b5e4fae9cebb77f3ca4614fa0897a6143f6b8950
Forist <jittiwatchada@gmail.com>
2024-10-15 23:46:47 +0700

    just change somethings

--- src/main/java/net/forist/mccourse/MCCourseMod.java
+++ src/main/java/net/forist/mccourse/MCCourseMod.java
@@ -1,21 +1,21 @@
 package net.forist.mccourse;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.client.Minecraft;
-import net.minecraft.world.level.block.Blocks;
+import net.forist.mccourse.block.ModBlocks;
+import net.forist.mccourse.item.ModCreativeModeTabs;
+import net.forist.mccourse.item.ModItems;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.ForgeRegistries;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -29,6 +29,12 @@ public class MCCourseMod
     public MCCourseMod()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModCreativeModeTabs.register(modEventBus);
+
+        ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
+
 
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
@@ -48,7 +54,19 @@ public class MCCourseMod
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
+        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS)
+        {
+            event.accept(ModBlocks.ALEXANDRITE_BLOCK);
+            event.accept(ModBlocks.RAW_ALEXANDRITE_BLOCK);
+            event.accept(ModBlocks.ALEXANDRITE_ORE);
+            event.accept(ModBlocks.DEEPSLATE_ALEXANDRITE_ORE);
+        }
 
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS)
+        {
+            event.accept(ModItems.ALEXANDRITE);
+            event.accept(ModItems.RAW_ALEXANDRITE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/net/forist/mccourse/item/ModCreativeModeTabs.java
@@ -0,0 +1,40 @@
+package net.forist.mccourse.item;
+
+import net.forist.mccourse.MCCourseMod;
+import net.forist.mccourse.block.ModBlocks;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModCreativeModeTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TAB =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MCCourseMod.MOD_ID);
+
+    //Can Copy All of this to create another , but don't forget to rename
+    public static final RegistryObject<CreativeModeTab> COURSE_TAB = CREATIVE_MODE_TAB.register("course_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.URIEL_MOCHI.get())).title(Component.translatable("creativetab.course_tab")).displayItems((itemDisplayParameters, output) ->
+            {
+                output.accept(ModItems.ALEXANDRITE.get());
+                output.accept(ModItems.RAW_ALEXANDRITE.get());
+
+                output.accept(ModBlocks.ALEXANDRITE_BLOCK.get());
+                output.accept(ModBlocks.RAW_ALEXANDRITE_BLOCK.get());
+                output.accept(ModBlocks.ALEXANDRITE_ORE.get());
+                output.accept(ModBlocks.DEEPSLATE_ALEXANDRITE_ORE.get());
+                output.accept(ModBlocks.END_STONE_ALEXANDRITE_ORE.get());
+                output.accept(ModBlocks.NETHER_ALEXANDRITE_ORE.get());
+
+
+                output.accept(ModItems.URIEL_MOCHI.get());
+                output.accept(ModItems.LEVINA_MOCHI.get());
+
+            }).build());
+
+    public static void register(IEventBus eventBus){
+        CREATIVE_MODE_TAB.register(eventBus);
+    }
+}
